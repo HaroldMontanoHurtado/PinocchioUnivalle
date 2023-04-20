ea91fa48c0c4e82503838c2616c041aaa9a6b81a
HaroldMontanoHurtado <harold.montano@correounivalle.edu.co>
2023-04-19 23:16:47 -0500

    Se agrego la libreria custom tkinter

---
+++ src/GUI.py
@@ -0,0 +1,9 @@
+from customtkinter import CTk, CTkFrame
+
+root = CTk()
+
+root.geometry('500x600+350+20')
+root.minsize(480, 500)
+root.title('Pinocchio - Inteligencia Artificial')
+
+root.mainloop()
